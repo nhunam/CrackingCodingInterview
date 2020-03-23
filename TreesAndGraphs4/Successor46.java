ba5dede703f6eabb8c1a84bd3dc2ad5003a0b652
nhuthanhnam@gmail.com <nhuthanhnam@gmail.com>
2020-03-23 15:39:54 +0700

    update 4.6

---
+++ TreesAndGraphs4/Successor46.java
@@ -0,0 +1,57 @@
+package TreesAndGraphs4;
+/*
+ * Successor: Write an algorithm to fnd the "next" node (i.e., in-order successor) of a given node in a
+binary search tree. You may assume that each node has a link to its parent.
+ */
+
+public class Successor46 {
+
+	class TreeNode {
+		TreeNode parent = null;
+		TreeNode right;
+		TreeNode left;
+		int val;
+		
+		public TreeNode(int x) {
+			val = x;
+		}
+	}
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Successor46 s = new Successor46();
+		TreeNode r = s.new TreeNode(6);
+		r.parent = null;
+		r.left = s.new TreeNode(4);
+		r.left.parent = r;
+		r.right = s.new TreeNode(8);
+		r.right.parent = r;
+		
+		System.out.println(s.findNextInorder(r.right));
+		
+	}
+	
+	TreeNode findNextInorder(TreeNode n) {
+		// In-order: left, node, right
+		if (n == null) return null;
+		/* Found right children -> return leftmost node of right subtree */
+		if (n.right != null) {
+			return leftMostChild(n.right);
+		} else {
+			
+		}
+		
+	}
+	
+	TreeNode leftMostChild(TreeNode n) {
+		if (n == null)
+			return null;
+		while (n.left != null) {
+			n = n.left;
+		}
+		return n;
+	}
+	
+}
+
